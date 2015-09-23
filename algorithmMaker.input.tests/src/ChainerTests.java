df1d903374372a603faf0d9e4fedc74b60ea624e
dwightnaylor <dwight.naylor@sbcglobal.net>
2015-09-23 12:41:59 -0400

    Added a few tests

--- algorithmMaker.input.tests/src/ChainerTests.java
+++ algorithmMaker.input.tests/src/ChainerTests.java
@@ -8,6 +8,15 @@ import algorithmMaker.input.Atomic;
 public class ChainerTests {
 
 	@Test
+	public void testForFalsePositives() {
+		Chainer basicChainer = new Chainer(new Theorem(parseProperty("a(x)"), parseProperty("b(x)")));
+		basicChainer.chain(parseProperty("a(y)"), Theorem.GIVEN);
+		assertTrue("No false positives", !basicChainer.hasAtomic((Atomic) parseProperty("b(x)")));
+		assertTrue("No false positives", !basicChainer.hasAtomic((Atomic) parseProperty("c(x)")));
+		assertTrue("No false positives", !basicChainer.hasAtomic((Atomic) parseProperty("c(y)")));
+	}
+
+	@Test
 	public void testBasicChainer() {
 		Chainer basicChainer = new Chainer(new Theorem(parseProperty("a(x)"), parseProperty("b(x)")));
 		basicChainer.chain(parseProperty("a(y)"), Theorem.GIVEN);
@@ -29,6 +38,22 @@ public class ChainerTests {
 		basicChainer.chain(parseProperty("a(x)"), Theorem.GIVEN);
 		basicChainer.chain(parseProperty("b(x)"), Theorem.GIVEN);
 		assertTrue("Chainer can chain ANDed requirements", basicChainer.hasAtomic((Atomic) parseProperty("c(x)")));
+	}
+
+	@Test
+	public void testMultipleChaining() {
+		Chainer basicChainer = new Chainer(new Theorem(parseProperty("a(x)"), parseProperty("b(x)")),
+				new Theorem(parseProperty("b(x)"), parseProperty("c(x)")));
+		basicChainer.chain(parseProperty("a(y)"), Theorem.GIVEN);
+		assertTrue("Multiple chaining works", basicChainer.hasAtomic((Atomic) parseProperty("c(y)")));
+	}
+
+	@Test
+	public void testMultivarChaining() {
+		Chainer basicChainer = new Chainer(new Theorem(parseProperty("a(x,y) & b(y,z)"), parseProperty("c(x,z)")));
+		basicChainer.chain(parseProperty("a(d,e)"), Theorem.GIVEN);
+		basicChainer.chain(parseProperty("b(e,f)"), Theorem.GIVEN);
+		assertTrue("Multiple variable chaining works", basicChainer.hasAtomic((Atomic) parseProperty("c(d,f)")));
 	}
 
 }
