29b5e580a3162f7b4875063270d995eb5ef479c7
dwightnaylor <dwight.naylor@sbcglobal.net>
2015-10-21 18:47:56 -0400

    Added test skeleton for expression simplifier

---
+++ algorithmMaker.input.tests/src/SimplifierTests.java
@@ -0,0 +1,27 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import algorithmMaker.QuickParser;
+import algorithmMaker.input.Property;
+
+public class SimplifierTests {
+
+	@Test
+	public void testSimplifications() {
+		String[] originals = { "a(x) & a(x)", "a(x) & b(x) & c(x)" };
+		String[] simplified = { "a(x)", "a(x) & b(x) & c(x)" };
+		for (int i = 0; i < originals.length; i++) {
+			Property originalProperty = QuickParser.parseProperty(originals[i]);
+			// The simplified version goes here
+			Property simplifiedProperty = null;
+
+			if (!originalProperty.equals(simplifiedProperty))
+				System.err.println("\"" + originalProperty + "\" Should reduce to \"" + simplified[i]
+						+ "\" but it instead reduced to " + simplifiedProperty);
+
+			assertEquals(simplifiedProperty.toString(), simplified[i]);
+		}
+	}
+
+}
