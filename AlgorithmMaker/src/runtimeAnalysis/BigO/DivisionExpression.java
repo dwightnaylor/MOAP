23c3631c54dfd5c8cbaf495103f9debc92ce95a8
dwightnaylor <dwight.naylor@sbcglobal.net>
2016-02-19 18:19:24 -0500

    Added bigoparser and commented tests for it

--- AlgorithmMaker/src/runtimeAnalysis/BigO/DivisionExpression.java
+++ AlgorithmMaker/src/runtimeAnalysis/BigO/DivisionExpression.java
@@ -1,6 +1,6 @@
 package runtimeAnalysis.BigO;
 
-public abstract class DivisionExpression extends CompositeExpression{
+public class DivisionExpression extends CompositeExpression{
 
 	public DivisionExpression(Expression left, Expression right) {
 		super(left, right);
